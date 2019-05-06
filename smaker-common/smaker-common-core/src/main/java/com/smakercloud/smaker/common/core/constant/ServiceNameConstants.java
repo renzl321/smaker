94ae2a564cd41b2fbb5940533ac461d7675691fe
renzl <rengtao321@163.com>
2019-05-06 11:23:13 +0800

    信息完善

--- smaker-common/smaker-common-core/src/main/java/com/smakercloud/smaker/common/core/constant/ServiceNameConstants.java
+++ smaker-common/smaker-common-core/src/main/java/com/smakercloud/smaker/common/core/constant/ServiceNameConstants.java
@@ -14,18 +14,14 @@ package com.smakercloud.smaker.common.core.constant;
 
 /**
  * @author renzl
- * @date 2018年06月22日16:41:01
+ * @date 2019年06月22日16:41:01
  * 服务名称
  */
 public interface ServiceNameConstants {
-	/**
-	 * 认证服务的SERVICEID（zuul 配置的对应）
-	 */
+
 	String AUTH_SERVICE = "smaker-auth";
 
-	/**
-	 * UMPS模块
-	 */
+
 	String UMPS_SERVICE = "smaker-back";
 
 }
