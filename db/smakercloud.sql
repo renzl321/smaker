fa4bd9a68e844b4ff1c09e57dcfea8e2ba8cd587
renzl <rengtao321@163.com>
2019-05-09 18:52:21 +0800

    数据库完善..

--- db/smakercloud.sql
+++ db/smakercloud.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50723
  File Encoding         : utf-8
 
- Date: 02/01/2019 20:51:15 PM
+
 */
 DROP DATABASE IF EXISTS `smaker`;
 CREATE DATABASE  `smakercloud` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
